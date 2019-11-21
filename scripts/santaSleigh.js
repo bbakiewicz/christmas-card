514565c3387245aa35e92e69c2186c9f9890b6c5
Blazej Bakiewicz <b.bakiewicz@pkm.local>
2019-11-21 15:25:40 +0100

    added santa sleigh with animation

---
+++ scripts/santaSleigh.js
@@ -0,0 +1,12 @@
+var cardContainer = document.querySelector('.container');
+var moon = document.createElement('div');
+moon.className = 'moon';
+cardContainer.appendChild(moon);
+
+
+var santaSleigh = document.createElement('div');
+santaSleigh.className = 'sleigh';
+cardContainer.appendChild(santaSleigh);
+
+
+cardContainer.style.backgroundColor = '#316987';
