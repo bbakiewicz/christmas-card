5cc09fabf40d955ab6c515ee3f8a963857c13448
Blazej Bakiewicz <b.bakiewicz@pkm.local>
2019-11-21 15:54:26 +0100

    first slide finished

--- scripts/santaSleigh.js
+++ scripts/santaSleigh.js
@@ -1,4 +1,7 @@
-var cardContainer = document.querySelector('.container');
+var container = document.querySelector('.container');
+container.style.backgroundColor = '#316987';
+
+var cardContainer = document.querySelector('#animation__container');
 var moon = document.createElement('div');
 moon.className = 'moon';
 cardContainer.appendChild(moon);
@@ -8,5 +11,5 @@ var santaSleigh = document.createElement('div');
 santaSleigh.className = 'sleigh';
 cardContainer.appendChild(santaSleigh);
 
-
-cardContainer.style.backgroundColor = '#316987';
+var dialogue = document.querySelector('.dialogue');
+dialogue.style.color = '#fff';
